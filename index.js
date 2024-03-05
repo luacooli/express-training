d17cddcb5bec157c0c9ce5e960c27e3ca486246a
Luana Correia <luacooli@gmail.com>
2024-03-05 11:15:56 -0300

    feat: making a request to the internet

---
+++ index.js
@@ -0,0 +1,31 @@
+const https = require('https')
+const fs = require('fs')
+
+const options = {
+  hostname: 'pt.wikipedia.org',
+  port: 443,
+  path: 'wiki/Anarquismo',
+  method: 'GET',
+}
+
+const request = https.request(options, (res) => {
+  let responseBody = ''
+  res.setEncoding('UTF-8')
+
+  res.on('data', (chunk) => {
+    console.log('-----chunk', chunk.length)
+    responseBody += chunk
+  })
+
+  res.on('end', () => {
+    fs.writeFile('cher.html', responseBody, (err) => {
+      if (err) {
+        throw err
+      }
+
+      console.log('file dowloaded')
+    })
+  })
+})
+
+request.end()
